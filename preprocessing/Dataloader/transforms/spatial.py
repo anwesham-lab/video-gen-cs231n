03f3ca4c2237c98e39fc73de7949d805cade7d3b
anwesham-lab <anwesham@stanford.edu>
2023-05-13 01:47:42 -0700

    rename folder and then add train/main fixes

--- preprocessing/Dataloader/transforms/spatial.py
+++ preprocessing/Dataloader/transforms/spatial.py
@@ -10,6 +10,29 @@ try:
 except ImportError:
     accimage = None
 
+class Compose(object):
+    """Composes several transforms together.
+    Args:
+        transforms (list of ``Transform`` objects): list of transforms to compose.
+    Example:
+        >>> transforms.Compose([
+        >>>     transforms.CenterCrop(10),
+        >>>     transforms.ToTensor(),
+        >>> ])
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        for t in self.transforms:
+            img = t(img)
+        return img
+
+    def randomize_parameters(self):
+        for t in self.transforms:
+            t.randomize_parameters()
+
 class ToTensor(object):
     """Convert a ``PIL.Image`` or ``numpy.ndarray`` to tensor.
     Converts a PIL.Image or numpy.ndarray (H x W x C) in the range
--- preprocessing/scripts/prepare_data.sh
+++ preprocessing/scripts/prepare_data.sh
@@ -9,4 +9,6 @@ python3 utils/classify_video.py $ORIGIN_PATH $CLASSIFY_PATH
 python3 utils/ucf_jpeg.py $CLASSIFY_PATH $VIDEO_PATH
 python3 utils/n_frames.py $VIDEO_PATH
 
+python3 utils/ucf101_json.py $ANNO_PATH
+
 rm -rf $ORIGIN_PATH $CLASSIFY_PATH
